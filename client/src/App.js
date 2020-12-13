319f3e8f3627a7ee4987e38f5803d6cb347c9576
DharwinJRuppa <dharwinruppa1422@gmail.com>
2020-12-13 20:52:21 +0530

    dwin-library

---
+++ client/src/App.js
@@ -0,0 +1,98 @@
+
+import axios from "axios";
+import { useState } from "react";
+import { Card } from "react-bootstrap";
+import "./App.css";
+
+function App() {
+  const [book, setBook] = useState("");
+  const [result, setResult] = useState([]);
+  const [apikey, setApikey] = useState(
+    "AIzaSyBC0txMLdecUflrK3FnpMgwKcazyyjGzMg"
+  );
+  const handleChange = (event) => {
+    const book = event.target.value;
+    setBook(book);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .get(
+        "https://www.googleapis.com/books/v1/volumes?q=" +
+          book +
+          ":keyes&key=" +
+          apikey +
+          "&maxResults=40"
+      )
+      .then((data) => {
+        console.log(data.data.items);
+        setResult(data.data.items);
+      });
+  };
+  return (
+    <div className="App">
+      <div class="container my-4">
+        <div class="row">
+          <div class="col-lg-12 text-center text-monospace">
+            <h1>DWin's Library</h1>
+          </div>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div class="input-group mb-3">
+            <input
+              type="text"
+              onChange={handleChange}
+              class="form-control bg-warning"
+              placeholder="Enter a Book Name"
+              aria-label="Enter a Book Name"
+              aria-describedby="button-addon2"
+            ></input>
+            <div class="input-group-append">
+              <button
+                class="btn btn-outline-success"
+                type="submit"
+                id="button-addon2"
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
+        <div className="row">
+          {result.map((book) => (
+            <div className="col-lg-3">
+              <Card style={{ marginTop: "10px" }} class="bg-warning">
+                <a href={book.volumeInfo.previewLink} target="_blank">
+                  <Card.Img
+                    variant="top"
+                    src={
+                      book.volumeInfo.imageLinks !== undefined
+                        ? book.volumeInfo.imageLinks.thumbnail
+                        : ""
+                    }
+                    alt={book.volumeInfo.title} class="rounded img-fluid"
+                   
+                  />
+                </a>
+                <Card.Body className="tiles">
+                  <p className="card-title">
+                    <b>Title: </b>
+                    {book.volumeInfo.title}
+                  </p>
+
+                  <p className="card-title">
+                    <b>Author: </b>
+                    {book.volumeInfo.authors}
+                  </p>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+//set port
+var port = process.env.PORT || 8080;
+app.use(express.static(path.join(__dirname, "client/build")));
+//routes
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+app.listen(port, () => {
+  console.log("app running");
+});
